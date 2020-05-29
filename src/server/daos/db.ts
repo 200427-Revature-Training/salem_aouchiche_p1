fc74231df76cfab56bbaba164c7846a501247467
Salem Aouchiche <salem.aouchiche@gmail.com>
2020-05-29 00:48:12 -0700

    first commit

---
+++ src/server/daos/db.ts
@@ -0,0 +1,16 @@
+import { Pool } from 'pg';
+
+//const log = bunyan.createLogger({name: "Project_P1"});
+export const db = new Pool({
+    database: 'postgres',
+    host:process.env.NODE_APP_URL,
+    port: 5432,
+    user:process.env.NODE_PROJECT_ROLE,
+    password:process.env.NODE_APP_PASS
+});
+
+//console.log(JSON.stringify(db))
+
+db.on('connect', (client) => {
+    client.query(`SET search_path TO my_schema, project1`);
+});
